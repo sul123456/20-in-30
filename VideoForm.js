29899a33e5cce0ce4b86b087cb84aa9e9ce1fa0c
sul123456 <sulbhaaneja@gmail.com>
2026-09-26 11:29:07 +0530

    Rename delivery date to actual delivery date

--- VideoForm.js
+++ VideoForm.js
@@ -293,7 +293,7 @@ export default function VideoForm({ videos, statuses, L, saveVideo, openId, onOp
             {october && <div className="field"><label htmlFor="f_planned_delivery">Planned delivery date</label><input className="input" id="f_planned_delivery" type="date" value={form.planned_delivery_date} onChange={(e) => set("planned_delivery_date", e.target.value)} /></div>}
             {october && <div className="field"><label htmlFor="f_brand_sr">Brand SR</label><input className="input" id="f_brand_sr" value={form.brand_sr} onChange={(e) => set("brand_sr", e.target.value)} placeholder="Brand SR / approver" /></div>}
             <div className="field">
-              <label htmlFor="f_delivery">Delivery date</label>
+              <label htmlFor="f_delivery">Actual Delivery Date</label>
               <input className="input" id="f_delivery" type="date" value={form.delivery_date} onChange={(e) => set("delivery_date", e.target.value)} />
             </div>
             <div className="field">
